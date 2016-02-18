2fc6cb176a753647cedba6d5c187c66ac934656b
José María Delgado <MacbookPro@MacBook-Pro-de-Jose.local>
2016-02-18 00:32:35 +0000

    Added Spring Security Core

--- grails-app/controllers/grailsdemo/HomeController.groovy
+++ grails-app/controllers/grailsdemo/HomeController.groovy
@@ -1,8 +1,19 @@
 package grailsdemo
 
+import com.grailsDemo.User
+import grails.plugin.springsecurity.annotation.Secured
+
+@Secured('PermitAll')
 class HomeController {
 
+    def SpringSecurityService
+
     def index() {
-        render(view: "/home")
+        if (SpringSecurityService.isLoggedIn()) {
+            User currentUser = SpringSecurityService.getCurrentUser()
+            render(view: "/home", model: [user: currentUser])
+        } else {
+            redirect(uri: "/")
+        }
     }
 }
